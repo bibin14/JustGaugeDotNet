efff41327e49ddc23ef96ed01e408b5c40d654c2
bibin14 <bibin14@gmail.com>
2017-04-13 19:06:01 +0300

    Added GaugeSector Class

---
+++ JustGaugeDotNet/GaugeSector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustGaugeDotNet
+{
+    class GaugeSector
+    {
+        public Color Color { get; set; }
+        public int HighValue { get; set; }
+        public int LowValue { get; set; }
+    }
+}
